680def4eec2d291926dc23dbcbaa42c4f390d5f0
thanhlam <lam.nguyen@pascal.studio>
2024-03-24 16:54:14 +0700

    create server modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
-import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 import { cn } from "@/lib/utils";
 import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
 import { extractRouterConfig } from "uploadthing/server";
 import { ourFileRouter } from "@/app/api/uploadthing/core";
+import { ModalProvider } from "@/components/providers/modal-provider";
 import "./globals.css";
 
 const font = Open_Sans({ subsets: ["latin"] });
@@ -33,6 +34,7 @@ export default function RootLayout({
              */
             routerConfig={extractRouterConfig(ourFileRouter)}
           />
+          <ModalProvider />
           <ThemeProvider attribute="class" defaultTheme="dark" enableSystem storageKey="discord-theme">
             {children}
           </ThemeProvider>
